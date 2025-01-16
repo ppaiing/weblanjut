bb4e44a062011507b0e6530cf1f01b33a4214989
unknown <you@example.com>
2025-01-16 21:54:10 +0700

    update

--- public/script.js
+++ public/script.js
@@ -1,39 +1,44 @@
-const url = "http://localhost:3000"
+const url = "http://localhost:3000";
 
 async function main() {
-  const data = await fetch(url+"/mahasiswa", {
-  })
-  const json = await data.json()
-  console.log(json)
+  const data = await fetch(url + "/mahasiswa", {});
+  const json = await data.json();
+  console.log(json);
 }
 
 async function login(username, password) {
-    const data = await fetch(url+"/Login", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify({username, password})
-    })
-    const json = await data.json()
-    if(data.status >= 400) return alert(json.message)
-    console.log(json)
-    alert("Berhasil Login")
-    return json
+  try {
+    const data = await fetch(url + "/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const json = await data.json();
+    if (data.status >= 400) {
+      alert("Username atau Password salah.");
+      return;
+    }
+    alert("Berhasil Login");
+    window.location.href = "./welcome.html";
+    return json;
+  } catch (error) {
+    alert("Terjadi kesalahan. Silakan coba lagi.");
+  }
 }
 
 async function register(username, password) {
-    const data = await fetch(url+"/register", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify({username, password})
-    })
-    const json = await data.json()
-    if(data.status >= 400) return alert(json.message)
-    alert("Berhasil Register")
-    console.log(json)
-    return json
+  const data = await fetch(url + "/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  const json = await data.json();
+  if (data.status >= 400) return alert(json.message);
+  alert("Berhasil Register");
+  console.log(json);
+  return json;
 }
-
